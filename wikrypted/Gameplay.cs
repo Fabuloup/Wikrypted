589d9fef1bfd650a740b30b812102106962abdfb
frichard0 <frichard@karibu-gestion.fr>
2025-02-03 15:21:42 +0100

    Fixed hint reveal condition

--- wikrypted/Gameplay.cs
+++ wikrypted/Gameplay.cs
@@ -47,7 +47,7 @@ public static class Gameplay
                 for (int i = 0; i < originalArticle.Description.Length; i++)
                 {
                     var letter = originalArticle.Description[i];
-                    if (!lookupTable.ContainsKey(letter) || lookupTable[letter] != letter || letter != encodedArticle.Description[i])
+                    if ((!lookupTable.ContainsKey(letter) && letter != encodedArticle.Description[i]) || lookupTable[letter] != letter)
                     {
                         lookupTable[letter] = letter;
                         break;
